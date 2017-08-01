e27b0ceef48174068e9561ad4015b16358d4fe4b
Sam Richard <sam@snug.ug>
2017-08-01 11:04:43 -0500

    :wrench: Update to Stage Fright
    Needs lots of styling and content, but it's all there

---
+++ js/main.js
@@ -0,0 +1,10 @@
+import stageFright from '../node_modules/stage-fright/src/js/lib/core';
+
+// Prism
+import '../node_modules/prismjs/prism';
+import '../node_modules/prismjs/components/prism-bash';
+import '../node_modules/prismjs/components/prism-javascript';
+import '../node_modules/prismjs/components/prism-scss';
+
+
+stageFright();
